947917967058006ab446ff4df9381decc593bec8
juankmiloh <juankmiloh@hotmail.com>
2021-11-05 10:26:06 -0500

    update repo

--- server.sh
+++ server.sh
@@ -3,4 +3,5 @@
 # -- COMANDOS PARA LEVANTAR SERVIDOR DE PRODUCCION VUEJES
 # 1. sudo npm install -g serve
 # 2. npm run build:prod
+# sudo chown azureuser frontend_unigrasas_facturacion/# Dar permisos de propietario de la carpeta al usuario LINUX
 serve -s dist -l 9527
